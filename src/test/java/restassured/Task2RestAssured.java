46a4dcf2561c903b7ca034d4e0837bcdc373d186
Adhi <adhistr@gmail.com>
2025-02-18 10:18:31 +0700

    Task Rest Assured 2

--- src/test/java/restassured/Task2RestAssured.java
+++ src/test/java/restassured/Task2RestAssured.java
@@ -6,7 +6,29 @@ import io.restassured.specification.RequestSpecification;
 
 public class Task2RestAssured {
     public static void main(String[] args) {
-        getAllProducts();
+        //getAllProducts();
+        getSingleProduct();
+    }
+    public static void getSingleProduct(){
+        /*
+         * 'https://dummyjson.com/products/1'
+         */
+
+
+         RestAssured.baseURI = "https://dummyjson.com";
+         RequestSpecification requestSpecification = RestAssured
+                                                     .given();
+
+        Response response = requestSpecification
+                                .log()
+                                .all()
+                                .pathParam("idProduct", 1)
+                                .pathParam("path", "products")
+                            .when()
+                                .get("{path}/{idProduct}");
+
+
+        System.out.println("single Product " + response.asPrettyString()); 
     }
     
     public static void getAllProducts(){
